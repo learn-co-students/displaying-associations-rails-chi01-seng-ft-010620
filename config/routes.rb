c59f661bd3248b4ff2cc91ea0b76b894465b7057
Cesar <cesarmota@ymail.com>
2020-02-05 11:35:39 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories, :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
+
+
+# Prefix Verb  URI Pattern                    Controller#Action
+# categories GET   /categories(.:format)          categories#index
+#            POST  /categories(.:format)          categories#create
+# new_category GET   /categories/new(.:format)      categories#new
+# edit_category GET   /categories/:id/edit(.:format) categories#edit
+#   category GET   /categories/:id(.:format)      categories#show
+#            PATCH /categories/:id(.:format)      categories#update
+#            PUT   /categories/:id(.:format)      categories#update
+#      posts GET   /posts(.:format)               posts#index
+#            POST  /posts(.:format)               posts#create
+#   new_post GET   /posts/new(.:format)           posts#new
+#  edit_post GET   /posts/:id/edit(.:format)      posts#edit
+#       post GET   /posts/:id(.:format)           posts#show
+#            PATCH /posts/:id(.:format)           posts#update
+#            PUT   /posts/:id(.:format)           posts#update
